ef81a3e60730c651e8ba5ed0c1f391e3f62fc535
Hukyc <nukutac@gmail.com>
2023-04-01 06:36:31 +0300

    Toasts and logs on every lifecycle created

--- app/src/main/java/com/example/fragments_practice3/SecondFragment.java
+++ app/src/main/java/com/example/fragments_practice3/SecondFragment.java
@@ -4,16 +4,57 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class SecondFragment extends Fragment {
+    private String TAG = "lifeCycle";
+
     public SecondFragment() {
         super(R.layout.fragment_second);
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
     }
+    @Override
+    public void onStart() {
+        Toast.makeText(getActivity(), "onStart", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Toast.makeText(getActivity(), "onResume", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Toast.makeText(getActivity(), "onPause", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Toast.makeText(getActivity(), "onStop", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
 }
