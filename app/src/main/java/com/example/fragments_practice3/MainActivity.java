4e9e95c13644a0ee4802cf7abee915fab42352f0
Hukyc <nukutac@gmail.com>
2023-04-01 06:17:52 +0300

    All fragments now contain data from previous activities

--- app/src/main/java/com/example/fragments_practice3/MainActivity.java
+++ app/src/main/java/com/example/fragments_practice3/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SecondFragment enteringFragment = new SecondFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, enteringFragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/fragments_practice3/ThirdFragment.java
+++ app/src/main/java/com/example/fragments_practice3/ThirdFragment.java
@@ -10,18 +10,10 @@ import android.view.ViewGroup;
 
 public class ThirdFragment extends Fragment {
     public ThirdFragment() {
-        // Required empty public constructor
+        super(R.layout.fragment_third);
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 }
